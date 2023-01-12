61f640d0a68371641635f84dc28f0e8405d1e835
johnobodai <j.obodai@alustudent.com>
2023-01-12 03:31:20 +0000

    trying ruby

--- hello.rb
+++ hello.rb
@@ -4,4 +4,40 @@ this is my first ruby programme
 
 =end
 puts "Hello, World!"
-print 'hello word'
+print 'hello world'
+puts
+puts '##########'
+num = 8
+
+if num > 4
+
+  puts "4"
+
+  if num < 7
+
+    puts "7"
+
+    if num == 8
+
+      puts "8"
+
+    end
+
+  end
+
+end
+puts '##########'
+x = 5
+
+unless x < 8
+
+  x += 3
+
+else
+
+  x += 2
+
+end
+
+puts x
+
