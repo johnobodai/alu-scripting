ec52dbc1de1198af9fd0bd37f70711e2ab0dbc65
johnobodai <j.obodai@alustudent.com>
2023-01-12 07:15:13 +0000

    Ruby

--- hello.rb
+++ hello.rb
@@ -2,7 +2,7 @@
 
 this is my first ruby programme
 
-=end
+
 puts "Hello, World!"
 print 'hello world'
 puts
@@ -40,4 +40,46 @@ else
 end
 
 puts x
+puts '##########'
+x = 5
 
+unless x < 8
+
+  x += 3
+
+else
+
+  x += 2
+
+end
+
+puts x
+
+x = 0
+
+for i in 2...4
+
+  x+=i
+
+end
+
+puts x
+
+a = [3, 7, 5]
+
+b = [1, 3]
+
+res = a - b
+
+puts res[0]
+=end
+
+x = 8
+
+x /= 2
+
+y = 1
+
+x.times {y+=2}
+
+puts y
